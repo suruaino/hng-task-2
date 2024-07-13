14be0d8a7fe8728dbecfccbc02dc408af6a17c46
SURU EMMANUEL <suruaino@gmail.com>
2024-07-13 07:47:47 +0100

    debugged display error

--- src/components/main/Product.jsx
+++ src/components/main/Product.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const Product = ({ product, addToCart }) => {
-  const organizationId ="27f6387b9d7b4f4d823cf6c910de5825";
-  const appId = 'AJ5T733PD8P1NHF';
-  const apiKey = '2f8b45bc903246de81dfb434c5f3e4ed20240713011223927260';
-  const url = `https://api.example.com/products/${productId}`;
+  // const organizationId ="27f6387b9d7b4f4d823cf6c910de5825";
+  // const appId = 'AJ5T733PD8P1NHF';
+  // const apiKey = '2f8b45bc903246de81dfb434c5f3e4ed20240713011223927260';
+  // const url = `https://api.example.com/products/${productId}`;
 
   return (
     <div className="product">
@@ -16,7 +16,7 @@ const Product = ({ product, addToCart }) => {
           <p className='product__price'>#{product.price}</p>
         </div>
       </div>
-      <button onClick={() => addToCart(product)}>Add to Cart</button>
+      <button onClick={() => addToCart(product)}>Add to Cart </button>
     </div>
   );
 };
